55a786a4dcc9713132f5a0c3a9f0a3dba27461c7
Daniel Berlin <dberlin@dberlin.org>
2019-03-30 22:53:48 -0700

    Missing file

---
+++ src/vscode-util.ts
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Daniel Berlin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+// Utilities that require vscode imports
+import * as vscode from "vscode";
+import { DocumentSymbolManager } from "./DocumentManager";
+import { SymbolInfo } from "./SymbolInfo";
+
+/**
+ * Return symbol information (if we have any) for a given name in the document.
+ *
+ * @param document - VSCode document the symbol is in.
+ * @param symbolName - Symbol to try to find information about.
+ */
+export function getSymbolByName(
+  document: vscode.TextDocument,
+  symbolName: string
+) {
+  let tries = DocumentSymbolManager.getTriesForDocument(document);
+  if (!tries) return null;
+  return tries.getSymbol(symbolName);
+}
+/**
+ * Convert a (document, position) pair into a word in the document.
+ *
+ * @param document - VSCode document the position is for.
+ * @param position - Position in document to get word for.
+ */
+export function getWordForPosition(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): string | null {
+  let wordRange = document.getWordRangeAtPosition(position);
+  if (!wordRange) {
+    return null;
+  }
+  return document.getText(wordRange);
+}
+
+/**
+ * Return symbol information (if we have any) for a given position in the document.
+ *
+ * @param document - VSCode document the position is for.
+ * @param position - Position in document to get symbol for.
+ */
+export function getSymbolForPosition(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): SymbolInfo | null {
+  let wordText = getWordForPosition(document, position);
+  if (!wordText) return null;
+  return getSymbolByName(document, wordText);
+}
